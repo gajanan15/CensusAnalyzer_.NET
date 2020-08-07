43407d27ae75f926294c04c0baac22b66eac1912
gajanan15 <gajanandesai97@gmail.com>
2020-08-07 23:53:41 +0530

    Loaded Indian State Census Data From CSV File

---
+++ CensusAnaluzers/CensusAnalyserException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalysers
+{
+    public class CensusAnalyserException : Exception
+    {
+        public enum ExceptionType
+        { 
+            FILE_NOT_FOUND,INVALID_TYPE,INVALID_DELIMITER,INVALID_HEADER
+        }
+
+       public ExceptionType type;
+       
+        public CensusAnalyserException(String message,ExceptionType type) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
